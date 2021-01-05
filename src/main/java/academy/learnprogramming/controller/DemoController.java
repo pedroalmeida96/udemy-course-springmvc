2e9d747ef02b017dee55fdc6ca290913136b4716
pedroalmeida96 <43607455+pgoaa@users.noreply.github.com>
2021-01-05 19:41:35 +0000

    10. Model and Model Attributes

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,9 +1,13 @@
 package academy.learnprogramming.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
     // http://localhost:8080/todo-list/hello
@@ -14,11 +18,19 @@ public class DemoController {
     }
 
     // http://localhost:8080/todo-list/welcome
-    // prefix + name + suffix
-    // /WEB-INF/view/ + welcome + .jsp
 
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("user", "pedro");
+        log.info("model {}", model);
+        // prefix + name + suffix
+        // /WEB-INF/view/ + welcome + .jsp
         return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage called()");
+        return "Welcome to this DemoApplication";
     }
 }
